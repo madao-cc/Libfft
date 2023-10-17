4762bc7c8a0d16419ce5d282425eb8abe3d6722d
madao-cc <143662001+madao-cc@users.noreply.github.com>
2023-10-17 17:19:51 +0100

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: madao-da <madao-da@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 11:00:53 by madao-da          #+#    #+#             */
+/*   Updated: 2023/10/09 18:45:18 by madao-da         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	n;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (little[i] == '\0')
+		return ((char *)big);
+	n = ft_strlen(little);
+	while (big[i] && i < len)
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (big[i + j] == little[j] && i + j < len)
+			{
+				if (little[j + 1] == '\0')
+					return ((char *)big + i);
+				j++;
+			}
+		}
+		i++;
+	}
+	return (0);
+}
